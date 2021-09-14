140b64ac9e422d5a4b84619fb63a48d1c736b1c1
elinln <linneaelinnilsson@gmail.com>
2021-09-14 14:43:13 +0200

    some small functions

--- public/logic.js
+++ public/logic.js
@@ -2,9 +2,36 @@ let socket = io()
 let name = ""
 
 window.onload = () => {
-    name = prompt("Vad heter du?")
+    name = prompt("Enter your name!")
+
+    if (name === "") {
+        alert("You have to enter a name to join the room...")
+        name = prompt("Just enter your name already...")
+    }
 
     socket.emit("joined", { name })
+    const list = document.getElementById("messages")
+    let listItem = document.createElement("li")
+    list.appendChild(listItem)
+
+    const time = new Date().getHours();
+
+    if (time < 17) {
+        listItem.innerText = "Good day " + name + "!"
+
+    } else if (time < 23) {
+        listItem.innerText = "Good evening " + name + "!"
+
+    } else if (time > 23) {
+        listItem.innerText = "You should get some sleep " + name + "!"
+
+    } else if (time > 5) {
+        listItem.innerText = "Good morning " + name + "!";
+
+    } else {
+        listItem.innerText = "Good day " + name + "!";
+    };
+
 }
 
 // message output
@@ -13,13 +40,14 @@ socket.on('message', (incoming) => {
     let listItem = document.createElement("li")
     listItem.innerText = incoming.name + ": " + incoming.message
     list.appendChild(listItem)
+    scrolling() // scrolling
 })
 
 // gif output
 socket.on('gif', (incoming) => {
-    console.log("client side")
     const list = document.getElementById("messages")
     const fig = document.createElement("figure")
+    fig.style.padding = "6px 13px"
     const user = document.createElement("li")
     user.innerText = incoming.name + ": "
     let listItem = document.createElement("img")
@@ -27,18 +55,20 @@ socket.on('gif', (incoming) => {
     fig.appendChild(listItem)
     list.appendChild(user)
     list.appendChild(fig)
-
+    scrolling(); // scrolling
 })
 
 // sending-message function
 async function sendMessage() {
     const input = document.getElementById("message")
     const message = input.value
+    scrolling(); // scrolling
 
     // gif output
     if (message.substr(0, 1) === '/') {
         const gif = await searchGif(message);
         socket.emit('gif', { name, gif })
+        input.value = ""
         return
     }
 
@@ -46,17 +76,28 @@ async function sendMessage() {
     socket.emit('message', { name, message })
 }
 
+// send message with enter-button
+let sendingMessage = document.getElementById("message");
+sendingMessage.addEventListener("keyup", function (event) {
+    if (event.which === 13) {
+        event.preventDefault();
+        document.getElementById("button").click();
+        scrolling(); // scrolling
+    } if (sendingMessage === "") {
+        return
+    }
+});
 
-// joining
+// joining user
 socket.on("joined", (user) => {
-    console.log(" joined the room")
+    console.log("someone joined the room")
     const list = document.getElementById("messages")
     let listItem = document.createElement("li")
     listItem.innerText = user.username + " joined the room!"
     list.appendChild(listItem)
 })
 
-// disconnect
+// disconnected user
 socket.on('userLeft', (user) => {
     const list = document.getElementById("messages")
     let listItem = document.createElement("li")
@@ -64,35 +105,55 @@ socket.on('userLeft', (user) => {
     list.appendChild(listItem)
     console.log("disconnect right")
 })
+
 let typing = false;
 
-//Check to see if someone is typing
+// check to see if someone is typing
 function timeoutTypingFunction() {
     typing = false;
     socket.emit("typing", { typing: typing, userName: name });
-  }
-  
-  function someoneIsTyping() {
+}
+
+function someoneIsTyping() {
     if (typing === false) {
-      typing = true;
-      //Send to server that someone is typing
-      socket.emit("typing", { typing: typing, userName: name });
-      timeout = setTimeout(timeoutTypingFunction, 4000);
+        typing = true;
+        //Send to server that someone is typing
+        socket.emit("typing", { typing: typing, userName: name });
+        timeout = setTimeout(timeoutTypingFunction, 3000);
     } else {
-      clearTimeout(timeout);
-      timeout = setTimeout(timeoutTypingFunction,4000);
+        clearTimeout(timeout);
+        timeout = setTimeout(timeoutTypingFunction, 3000);
     }
-  }
-  
-  //Listens from server if someone is typing
-  socket.on("typing", (typing) => {
+}
+
+// Listens from server if someone is typing
+socket.on("typing", (typing) => {
     const typingBox = document.getElementById("typing");
     if (typing.typing) {
-      const userTyping = document.createElement("li");
-      userTyping.style.listStyle = "none";
-      userTyping.innerText = `${typing.userName} is typing...`;
-      typingBox.append(userTyping);
+        const userTyping = document.createElement("li");
+        userTyping.style.listStyle = "none";
+        userTyping.innerText = `${typing.userName} is typing...`;
+        typingBox.append(userTyping);
     } else {
-      typingBox.innerHTML = "";
+        typingBox.innerHTML = "";
     }
-  });
+});
+
+// too se the options in the datalist
+function datalistOptions() {
+    let inputField = document.getElementById("message");
+    let datalist = document.querySelector("datalist");
+    if (inputField.value.substr(0, 1) === "/") {
+        datalist.id = "gifOptions";
+    } else {
+        datalist.id = "";
+    }
+}
+
+// to see latest messages
+function scrolling() {
+    var scrollDown = document.getElementById("footer");
+    scrollDown.scrollIntoView(false);
+}
+
+scrolling()
